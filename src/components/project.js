db28400acf557b02d938f306b36defa3507e47f3
Tanasit Nimpradit <tns.nccm@gmail.com>
2023-04-13 01:02:19 +0700

    Updates

--- src/components/project.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Project(){
-    return(
-        <section id="project" style={{minHeight:800}}>
-            <h1 className="text-shaded fs-1">Experience:</h1>
-        </section>
-    );
-}
